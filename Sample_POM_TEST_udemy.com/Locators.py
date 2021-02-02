2200a8cd865434d2f28698384941ec9b826d8385
Krzysztof Więckowski <krzyszwiec@wp.pl>
2021-02-02 11:11:42 +0100

    Delete

--- Sample_POM_TEST_udemy.com/Locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class MainPageLocators(object):
-    """A class for main page locators. All main page locators should come here"""
-    GO_BUTTON = (By.CSS_SELECTOR, "[type='submit']")
-    BUTTON_2 = (By.CSS_SELECTOR, "[class='udlite-btn udlite-btn-medium udlite-btn-secondary udlite-heading-sm']")
